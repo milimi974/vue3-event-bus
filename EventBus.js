b9c40cc8e6695d0aa85b092b185e5fa7a9389dab
milimi974 <milimi974@users.noreply.github.com>
2022-02-25 23:34:56 +0100

    Create EventBus.js

---
+++ EventBus.js
@@ -0,0 +1,32 @@
+/*!
+ * Vue3 EventBus manager v0.1.0
+ * (c) 2021-2021 Yohan Solon
+ * Released under the MIT License.
+ */
+class Event{
+    #EVENTS;
+    constructor() {
+        this.#EVENTS = {};
+    }
+
+    $emit(name, ...args){
+        if(this.#EVENTS[name] === undefined)
+            return;
+        for(const callback of this.#EVENTS[name])
+            callback(...args);
+    }
+
+    $on(name, callback){
+        if(typeof callback !== 'function')
+            return;
+        if(this.#EVENTS[name] === undefined)
+           this.#EVENTS[name] = [];
+        this.#EVENTS[name].push(callback);
+    }
+
+    $off(name){
+        this.#EVENTS[name] = [];
+    }
+}
+
+export const EventBus = new Event();
