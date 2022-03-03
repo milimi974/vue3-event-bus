7d2efe63609dff89587c46448c5edb3746015d58
milimi974 <milimi974@users.noreply.github.com>
2022-03-03 15:12:02 +0100

    Update view.js

--- src/view.js
+++ src/view.js
@@ -21,6 +21,13 @@ export default {
 
   },
   setup(props) {
+      ctx.data = {'first_name': 'john', 'last_name': 'Doe'};
+      let load = ()=>'Hello';
+      ctx.method = load;
+      ctx.method = function load2(){return 'World'};
+      ctx.method = function say(){
+            return this.first_name+' '+this.last_name+' say '+this.load()+' '+this.load2();
+      };
       EventBus.$on('empty-event', (value) => {
           /* data => ctx.data; methods => ctx.methods */
       });
@@ -31,6 +38,7 @@ export default {
       this.$nextTick(function () {
           // Code that will run only after the
           // entire view has been rendered
+          console.log(self.say());
           self.constructor();
           console.info('Empty Vue loaded ...');
       })
